14ae5be86fb648a3ad97897dfb519ccfbf7aa1f9
Rosano Coutinho <pub@xeari.com>
2022-01-18 12:01:11 -0500

    Lint

--- ui-test-_access.js
+++ ui-test-_access.js
@@ -11,7 +11,7 @@ Object.entries({
 	ROCOGlossaryZeroDataLink: '.ROCOGlossaryZeroDataLink',
 	ROCOGlossaryZeroDataBlurb: '.ROCOGlossaryZeroDataBlurb',
 }).map(function (e) {
-	return global[e.shift()]  = e.pop();
+	return global[e.shift()] = e.pop();
 });
 
 describe('ROCOGlossary_Access', function () {
